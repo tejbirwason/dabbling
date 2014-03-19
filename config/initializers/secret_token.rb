ba781d55c4b6929650245a5c7c554f7eebf50cbe
Tejbir Wason <tejbirwason@gmail.com>
2014-03-19 01:07:45 -0700

    Homie

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Homie::Application.config.secret_key_base = '8c3f85b6c49dfc346b0865cb650ca28c8c6ea20bbf9644a6b11950abc9ffe3a12daf4095f7083a1d8568077bb0deb6e210b2240ba79c452835fee79ef49def8e'
